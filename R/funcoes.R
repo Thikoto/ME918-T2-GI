815160f777a803237d8af73cf9b132d458c973d1
Thiago Makoto <165934984+Thikoto@users.noreply.github.com>
2024-10-06 15:58:55 -0300

    Update funcoes.R

--- R/funcoes.R
+++ R/funcoes.R
@@ -14,6 +14,7 @@
 #' X <- matrix(c(1, 2, 3, 4, 5), ncol = 1)
 #' y <- c(2, 4, 6, 8, 10)
 #' betas_est <- betas(dados = NULL, X, y)
+
 betas = function(dados, x, y) {
   # Validações de entrada
   if (!is.matrix(x) && !is.data.frame(x)) {
@@ -48,22 +49,24 @@ betas = function(dados, x, y) {
 #' Esta função calcula os valores preditos com base nas variáveis independentes
 #' e nos coeficientes estimados pelo modelo de regressão.
 #'
-#' @param dados Um data frame ou matriz contendo as variáveis independentes e dependentes.
 #' @param x Uma matriz das variáveis independentes. Deve ser numérica, sem a coluna do intercepto.
-#' @param y Um vetor numérico com os valores observados da variável dependente.
+#' @param betas Uma matriz com os valores dos betas da regressão. Recomenda-se o uso da função 'betas'
 #'
 #' @return Um vetor numérico com os valores preditos.
 #' @export
 #'
 #' @examples
 #' X <- matrix(c(1, 2, 3, 4, 5), ncol = 1)
-#' y <- c(2, 4, 6, 8, 10)
-#' y_pred <- preditos(dados = NULL, X, y)
+#' beta_p <- matrix(c(0.5, 2), nrow = 2, ncol = 1)
+#' y_pred <- preditos(X, beta_p)
 
 preditos = function(x, betas) {
   # Validações de entrada
   if (!is.matrix(x) && !is.data.frame(x)) {
     stop("x deve ser uma matriz ou data frame numérico.")
+  }
+  if (!is.matrix(betas) && !is.data.frame(betas)) {
+    stop("betas deve ser uma matriz ou data frame numérico.")
   }
   # Calcula os valores preditos usando os betas estimados
   x = cbind(1, x)
@@ -72,57 +75,62 @@ preditos = function(x, betas) {
   return(y_pred)
 }
 
+
 #' Calcula os resíduos do modelo de regressão linear
 #'
 #' Esta função calcula os resíduos (diferença entre os valores observados e os preditos)
 #' para um modelo de regressão linear.
 #'
-#' @param dados Um data frame ou matriz contendo as variáveis independentes e dependentes.
-#' @param x Uma matriz das variáveis independentes. Deve ser numérica, sem a coluna do intercepto.
 #' @param y Um vetor numérico com os valores observados da variável dependente.
+#' @param preditos Um vetor numérico com os valores preditos da variável dependente. Recomenda-se o uso da função 'preditos'
 #'
 #' @return Um vetor numérico com os resíduos.
 #' @export
 #'
 #' @examples
-#' X <- matrix(c(1, 2, 3, 4, 5), ncol = 1)
 #' y <- c(2, 4, 6, 8, 10)
-#' res <- residuos(dados = NULL, X, y)
+#' y_pred <- matrix(c(2.5, 4.6, 6.2, 8.9, 11), nrow = 5, ncol = 1)
+#' res <- residuos(y, y_pred)
+
 residuos = function(y, preditos) {
   # Validações de entrada
   if (!is.numeric(y)) {
     stop("y deve ser um vetor numérico.")
   }
-
+  if (!is.numeric(preditos)) {
+    stop("preditos deve ser um vetor numérico.")
+  }
   # Calcula os resíduos
   residuo = y - preditos
 
   return(residuo)
 }
 
+#residuos_obtido = residuos(mtcars$mpg, valores_preditos)
+
 #' Faz a predição de novos valores com base no modelo ajustado
 #'
 #' Esta função realiza predições para novos dados com base em um modelo de regressão ajustado manualmente.
 #'
-#' @param dados Um data frame ou matriz contendo as variáveis independentes e dependentes.
-#' @param novos_dados Uma matriz ou data frame numérico com os novos valores para predição.
+#' @param novox Uma matriz ou data frame numérico com os novos valores para predição.
 #' O número de colunas deve ser o mesmo da matriz X original (sem a coluna de intercepto).
-#' @param x Uma matriz das variáveis independentes utilizadas no ajuste do modelo original.
-#' @param y Um vetor numérico com os valores observados da variável dependente.
+#' @param betas Uma matriz com os valores dos betas da regressão. Recomenda-se o uso da função 'betas'
 #'
 #' @return Um vetor numérico com os valores preditos para os novos dados.
 #' @export
 #'
 #' @examples
-#' X <- matrix(c(1, 2, 3, 4, 5), ncol = 1)
-#' y <- c(2, 4, 6, 8, 10)
-#' novos_dados <- matrix(c(6, 7), ncol = 1)
-#' novas_predicoes <- predizer_novos_valores(dados = NULL, novos_dados, X, y)
+#' newx <- matrix(c(2, 5, 7), ncol = 1)
+#' beta_nx <- matrix(c(0.5, 2), nrow = 2, ncol = 1)
+#' novas_predicoes <- novos_preditos(newx, beta_nx)
 
 novos_preditos = function(novox, betas) {
   # Validações de entrada
   if (!is.matrix(novox) && !is.data.frame(novox)) {
-    stop("x deve ser uma matriz ou data frame numérico.")
+    stop("novox deve ser uma matriz ou data frame numérico.")
+  }
+  if (!is.matrix(betas) && !is.data.frame(betas)) {
+    stop("betas deve ser uma matriz ou data frame numérico.")
   }
   # Calcula os valores preditos usando os betas estimados
   novox = cbind(1, novox)
@@ -135,30 +143,39 @@ novos_preditos = function(novox, betas) {
 #'
 #' Esta função gera um gráfico comparando os valores observados com os valores preditos para o modelo de regressão.
 #'
-#' @param dados Um data frame ou matriz contendo as variáveis independentes e dependentes.
-#' @param x Uma matriz das variáveis independentes utilizadas no ajuste do modelo original.
 #' @param y Um vetor numérico com os valores observados da variável dependente.
+#' @param preditos Um vetor numérico com os valores preditos da variável dependente. Recomenda-se o uso da função 'preditos'
 #'
 #' @return Um gráfico de valores preditos vs observados.
 #' @export
 #'
 #' @examples
-#' X <- matrix(c(1, 2, 3, 4, 5), ncol = 1)
 #' y <- c(2, 4, 6, 8, 10)
-#' plot_predito_vs_observado(dados = NULL, X, y)
+#' y_pred <- matrix(c(2.5, 4.6, 6.2, 8.9, 11), nrow = 5, ncol = 1)
+#' grafico(y, y_pred)
 
-grafico = function(y, preditos, residuos){
+grafico = function(y, preditos){
+  if (!is.numeric(y)) {
+    stop("y deve ser um vetor numérico.")
+  }
+  if (!is.numeric(preditos)) {
+    stop("preditos deve ser um vetor numérico.")
+  }
+  residuo = y - preditos
+
   par(mfrow = c(2,2))
   plot(y , preditos, main = "Valores Observados vs Valores Preditos", xlab = "Valores Observados", ylab = "Valores Preditos")
   abline(0, 1, col = "deeppink2")
 
-  plot(preditos, residuos, main = "Residuos vs Valores Preditos",
+  plot(preditos, residuo, main = "Residuos vs Valores Preditos",
        xlab = "Valores Previstos", ylab = "Residuos")
   abline(h = 0, col = "#D9006E")
 
-  hist(residuos, main = "Histograma dos Residuos",
+  hist(residuo, main = "Histograma dos Residuos",
        xlab = "Residuos", breaks = 30, col = "pink2")
 
-  qqnorm(residuos, main = "QQ Plot dos Residuos")
-  qqline(residuos, col ="#D9006E")
+  qqnorm(residuo, main = "QQ Plot dos Residuos")
+  qqline(residuo, col ="#D9006E")
 }
+
+
