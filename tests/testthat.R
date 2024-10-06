54a76ded1ac6a95991521e6c5ef9e8a1d7ae5adb
MatheusRWodewotzki <m204345@dac.unicamp.br>
2024-10-06 16:11:55 -0300

    Update testthat.R

--- tests/testthat.R
+++ tests/testthat.R
@@ -9,44 +9,22 @@
 library(testthat)
 library(Regressao)
 
-test_check("Regressao")
+#test_check("Regressao")
 
-#data(mtcars)
-
-#constantes = rep(1, 32)
-
-betas_estimados2 = betas(mtcars, matrix(c(constantes, mtcars$hp, mtcars$disp), ncol = 3), mtcars$mpg)
-valores_preditos = preditos(matrix(c(mtcars$cyl, mtcars$hp, mtcars$disp), ncol = 3), betas = betas_estimados)
-residuos_obtido = residuos(mtcars$mpg, valores_preditos)
-#graficos = grafico(mtcars$mpg, valores_preditos, residuos_obtido)
+betas_estimados = betas(dados_teste, matrix(c(dados_teste$x1, dados_teste$x2, dados_teste$x3), ncol = 3), dados_teste$y_erro)
+valores_preditos = preditos(matrix(c(dados_teste$x1, dados_teste$x2, dados_teste$x3), ncol = 3), betas = betas_estimados)
+residuos_obtido = residuos(dados_teste$y_erro, valores_preditos)
+graficos = grafico(dados_teste$y_erro, valores_preditos, residuos_obtido)
 
 
 #install.packages("testthat")
 #library(testthat)
 
-test_that("erro com colunas constantes", {
-  expect_error(betas(mtcars, matrix(c(constantes, mtcars$hp, mtcars$disp), ncol = 3), mtcars$mpg), "singular", info = "Matriz X^T*X não é invertível.")
-})
-
-
-#x1 = seq(1:32)
-#x2 = 2*seq(1:32)
-
-betas(mtcars, matrix(c(x1, x2, mtcars$disp), ncol = 3), mtcars$mpg)
-
-test_that("erro quando a matriz X não possui posto completo", {
-  expect_error(betas(mtcars, matrix(c(x1, x2, mtcars$disp), ncol = 3), mtcars$mpg), "posto completo", info = "X não possui posto completo")
-})
-
+usethis::use_test("coluna_constante")
 
 ###########################################################
 
-coluna = 0.5*mtcars$mpg
-betas_estimados2 = betas(mtcars, matrix(coluna, ncol = 1), mtcars$mpg)
-valores_preditos2 = preditos(matrix(coluna, ncol = 1), betas = betas_estimados2)
-residuos_obtido2 = residuos(mtcars$mpg, valores_preditos2)
-test_that("residuos iguais a zero", {
-  expect_equal(as.vector(residuos_obtido2), rep(0, length(mtcars$mpg)), tolerance = 1e-8)
-})
+usethis::use_test("posto_incompleto")
 
-isZero(residuos_obtido2)
+###########################################################
+usethis::use_test("overfitting")
